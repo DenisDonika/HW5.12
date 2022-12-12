9ad3a8bf9345bcf25718ddc04e0c3747723036b7
Denis <balance.donika@gmail.com>
2022-12-12 22:27:33 +0600

    HW5.12

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях
+
+int[] CreateArray(int length) //принимает размер массива и возвращает сгенерированный массив
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+        array[i] = new Random().Next(-100, 100);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int el in array) //этот цикл только для просмотров и тут нет индексов
+        Console.Write($"{el} ");
+    // for(int i =0; i < array.Length; i++)
+    //  Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int sum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+            sum = sum + array[i];
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите размер массива");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
+PrintArray(array);
+Console.WriteLine(sum(array));
---
+++ Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿///Задайте массив случайных вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] CreateArray(int length) //принимает размер массива и возвращает сгенерированный массив
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+        array[i] = new Random().Next(-100, 100);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int el in array) //этот цикл только для просмотров и тут нет индексов
+        Console.Write($"{el} ");
+    // for(int i =0; i < array.Length; i++)
+    //  Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int max(int[] array)
+{
+    array[i] = max;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) ;
+        array[i] = max;
+    }
+    return max;
+}
+
+int min(int[] array)
+{
+    array[i] = min;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= min) ;
+        array[i] = min;
+    }
+    return min;
+}
+
+Console.WriteLine("Введите размер массива");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
+PrintArray(array);
+Console.WriteLine(max(array), min(array));
+Console.WriteLine($"{max - min}");
